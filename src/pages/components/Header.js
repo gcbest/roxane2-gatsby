9270366079be1d90aff50cd9005982d9393ab92c
Godfrey Best <godfrey.c.best@gmail.com>
2020-02-04 13:38:38 -0500

    first commit

---
+++ src/pages/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default () => 
+<header class="main-head">
+    <div class="head-container">
+
+    <h1 class="logo"><a href="index.html">
+        <img src="./assets/images/logo_transparent_background.png" alt=""/></a></h1>
+
+    <div class="banner-info">
+
+        <p class="phone-number"><span class="brand-color">Call:</span><a href="tel:+44 (0)1482 274551">+44 (0)1482
+            274551</a></p>
+
+        <ul class="top-social-media">
+
+        <li><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+
+        <li><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+
+        <li><a href="https://www.linkedin.com/in/dr-roxane-gervais/" target="_blank"><i
+                class="fa fa-linkedin"></i></a>
+        </li>
+        </ul>
+
+    </div>
+    </div>
+    <nav class="main-nav">
+    <ul>
+        <li><a class="nav-link" href="./index.html">Home</a></li>
+        <li><a class="nav-link" href="./about.html">About</a> </li>
+        <li><a class="nav-link" href="./publications.html">Publications</a></li>
+        <li><a class="nav-link" href="./contact.html">Contact</a></li>
+    </ul>
+    </nav>
+</header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,87 @@
 import React from "react"
+import Header from "./components/Header";
 
-export default () => <div>Hello world!</div>
+export default () => (<div>
+
+  <Header/>
+  <div class="hero-video-wrap">
+    <video autoplay muted loop>
+      <source src="./assets/videos/rotating-city.webm" type="video/mp4"/>
+    </video>
+    <div class="hero-overlay"></div>
+    <div class="hero-content">
+      <div class="hero-grid">
+        <img class="animated slideInUp" src="./assets//images/boris.png" alt="roxane"/>
+        <div class="hero-text">
+
+          <h1 class="name">Practical Psychology Consultancy Ltd.</h1>
+          <h3>
+            Providing focused solutions to improve and enhance workers’ well-being performance, and productivity; as
+            well as effective organisational functionality.
+          </h3>
+          <a href="./contact.html"><button class="btn">Contact Roxane</button></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="wrapper">
+    <article class="content">
+      <h1 class="content-header">Main article area</h1>
+      <p>
+        In this layout, we display the areas in source order for any screen
+        less that 500 pixels wide. We go to a two column layout, and then to a
+        three column layout by redefining the grid, and the placement of items
+        on the grid.
+      </p>
+      <br/>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit, sunt! Porro facilis similique quo mollitia
+        aspernatur minus architecto quia, quos, quae hic possimus doloribus, repudiandae reiciendis neque quasi tempore
+        animi! Iste, in maiores eveniet nulla, doloremque fugiat at expedita recusandae necessitatibus molestias
+        voluptates laborum temporibus a dolor enim. Corrupti, hic.
+      </p>
+    </article>
+    <aside class="side">
+      <ul>
+        <li><a href="./index.html">Home</a></li>
+        <li class="full-text"><a href="./about.html">About</a> </li>
+        <li class="short-text"><a href="./about.html">About</a> </li>
+        <li><a href="./publications.html">Publications</a></li>
+        <li><a href="./contact.html">Contact</a></li>
+      </ul>
+    </aside>
+    <div class="accreds">
+      <img src="./assets/images/chartered_psychologist_logo.png" alt="accred1"/>
+      <img src="./assets/images/hpc-reg-logo.png" alt="accred2"/>
+    </div>
+  </div>
+  <footer class="main-footer">
+    <div class="about">
+      <h2>About Roxane</h2>
+      Dr. Gervais is a highly goal-oriented professional with strong data analytic and interpretation experience. Highly
+      experienced in
+      research design, survey design, psychological assessment, organisational change, organisational development,
+      psychometrics, and career development. Extremely organised with the ability to manage multiple projects and meet
+      deadlines. Years of experience working in diverse teams and in international environments. A strong work ethic
+      combined with a commitment to excellence in all projects undertaken. Strong communication and presentation
+      skills.
+    </div>
+    <div class="posts">
+      <h2>Recent Posts</h2>
+      <ul>
+        <li>One</li>
+        <li>Two</li>
+        <li>Three</li>
+      </ul>
+    </div>
+    <div class="contact">
+      <h2>Contact Us</h2>
+      <p>Roxane Gervais</p>
+      <p>55 Old Trattford Rd, Hull Yorkshire, North of the Wall</p>
+    </div>
+  </footer>
+  <div class="copyright">
+    Copyright &copy; 2020 Roxane Gervais
+  </div>
+
+  </div>)
