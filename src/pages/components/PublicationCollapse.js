559a91dca4de1790bae4bd72f5fccd488396a37f
Godfrey Best <godfrey.c.best@gmail.com>
2020-02-07 09:18:12 -0500

    display panel beneath button

---
+++ src/pages/components/PublicationCollapse.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import publicationsStyles from "./styles/publications.module.css"
+
+export default ({details: {title, description, url}}) => {
+    const [open, setOpen] = useState(false);
+
+    const handleToggle = () => {
+        setOpen(!open);
+        getClassNames();
+    }
+
+    const getClassNames = () => {
+        if(open) return publicationsStyles.active;
+        return '';
+    }
+
+   return (
+    <div className={`${publicationsStyles.panelGroup}`}>
+            <button type="button" onClick={handleToggle} className={`${publicationsStyles.collapsible} ${getClassNames()}`}>{title}</button>
+            <div className={open ? `${publicationsStyles.panelCollapse}`: `${publicationsStyles.panelCollapse} ${publicationsStyles.panelClose}`}>
+                {description}
+                <p><a href={url} target="_blank" rel="noopener noreferrer" className={`${publicationsStyles.viewArticle}`}>Read the report</a></p>
+            </div>
+    </div>
+    );
+}
--- src/pages/components/Publications.js
+++ src/pages/components/Publications.js
@@ -1,100 +1,77 @@
-import React, {useState} from 'react';
-import publicationsStyles from "./styles/publications.module.css"
+import React from 'react';
+import PublicationCollapse from './PublicationCollapse';
 
 export default () => {
-    const [open, setOpen] = useState(true);
+    const publications = [
+        {
+            title: `The effects of strategic organisational change and practices on psychosocial risks`,
+            description: 'Organisational change is a useful process within an organisation, and could lead to improved practices and procedures. However, it tends to occur to a greater extent during periods of recession or economic turmoil. Regardless as to when it occurs, it is useful to explore the impacts of these changes on occupational safety and health (OSH). This article examines the relationship between various organisational practices (such as, lean production, downsizing and resizing, globalisation, outsourcing, continuous improvement, and process reengineering), and how they affect psychosocial risks in the workplace. In addition, the implications for workers’ health and wellbeing are outlined and discussed.',
+            url: 'http://oshwiki.eu/wiki/The_effects_of_organisational_practices_on_psychosocial_risks' 
+        },
+        {
+            title: `Prevention of work-related violence to staff in the education sector`,
+            description: 'One of the risks to which staffs in the education sector are exposed is violence. When workers are exposed to violence, it may impact adversely on their health and wellbeing. Additionally, this may affect their ability to function as educators. This article focuses mainly on third-party violence, i.e., that which is perpetrated against staff by pupils / students / parents, but provides as well some information on violence perpetrated by colleagues. It outlines the impact that work-related violence has on those who experience it, and presents good practice and prevention examples that are available for educational institutions and workers in this sector.',
+            url: 'http://oshwiki.eu/wiki/Prevention_of_work-related_violence_to_staff_in_the_education_sector' 
+        },
+        {
+            title: `Worker participation in the United Kingdom`,
+            description: '',
+            url: 'http://oshwiki.eu/wiki/Worker_participation_-_United_Kingdom' 
+        },
+        {
+            title: `Detachment and recovery after work: An overview`,
+            description: '',
+            url: 'http://oshwiki.eu/wiki/Detachment_and_recovery_after_work:_An_overview' 
+        },
+        {
+            title: `Presenteeism: An overview`,
+            description: '',
+            url: 'http://oshwiki.eu/wiki/Presenteeism:_An_overview' 
+        },
+        {
+            title: `Workaholism and the culture of competition: OSH consequences`,
+            description: '',
+            url: 'http://oshwiki.eu/wiki/Workaholism_and_the_culture_of_competition:_OSH_consequences' 
+        },
+        {
+            title: `The effects of strategic organisational change and practices on psychosocial risks`,
+            description: '',
+            url: 'http://oshwiki.eu/wiki/The_effects_of_organisational_practices_on_psychosocial_risks' 
+        },
+        {
+            title: `Prevention of work-related violence to staff in the education sector`,
+            description: '',
+            url: 'http://oshwiki.eu/wiki/Prevention_of_work-related_violence_to_staff_in_the_education_sector' 
+        },
+        {
+            title: `Worker participation in the United Kingdom`,
+            description: '',
+            url: 'http://oshwiki.eu/wiki/Worker_participation_-_United_Kingdom' 
+        },
+        {
+            title: `Detachment and recovery after work: An overview`,
+            description: '',
+            url: 'http://oshwiki.eu/wiki/Detachment_and_recovery_after_work:_An_overview' 
+        },
 
-    const handleToggle = () => {
-        setOpen(!open);
-        getClassNames();
-    }
 
-    const getClassNames = () => {
-        if(open) return publicationsStyles.active;
-        return '';
-    }
+
+        
+
+
+
+
+
+
+
+
+
+
+    ]
 
     return (
         <div className="publications-grid">
-        <div className="publication">
-
-
-        <button type="button" onClick={handleToggle} className={`${publicationsStyles.collapsible} ${getClassNames()}`}>Open Collapsible</button>
-        <div className={open ? `${publicationsStyles.panelCollapse}`: `${publicationsStyles.panelCollapse} ${publicationsStyles.panelClose}`}>
-            <p>Lorem ipsum...</p>
-        </div>
-
-        {/* <div className="panel-group">
-        <div className="panel panel-default">
-          <div className="panel-heading">
-            <h4 className="panel-title">
-                         
-              <button
-                onClick={handleToggle} >Collapsible list group</button>
-            </h4>
-          </div>
-            <div className={open ? `${publicationsStyles.panelCollapse}`: `${publicationsStyles.panelCollapse} ${publicationsStyles.panelClose}`}
-            >
-            <ul className="list-group">
-              <li className="list-group-item">One</li>
-              <li className="list-group-item">Two</li>
-              <li className="list-group-item">Three</li>
-            </ul>
-            </div>
-            </div>
-        </div> */}
-
-
-        <h3><a target="_blank" rel="noopener noreferrer"
-                    href="http://oshwiki.eu/wiki/The_effects_of_organisational_practices_on_psychosocial_risks">The
-                    effects of strategic organisational change and practices on
-                    psychosocial risks</a></h3>
-        </div>
-        <div className="publication">
-            <h3><a target="_blank" rel="noopener noreferrer"
-                    href="http://oshwiki.eu/wiki/Prevention_of_work-related_violence_to_staff_in_the_education_sector">Prevention
-                    of work-related violence to staff in the education
-                    sector</a></h3>
-        </div>
-        <div className="publication">
-            <h3><a target="_blank" rel="noopener noreferrer" href="http://oshwiki.eu/wiki/Worker_participation_-_United_Kingdom">Worker
-                    participation in the United Kingdom</a></h3>
-        </div>
-        <div className="publication">
-            <h3><a target="_blank" rel="noopener noreferrer"
-                    href="http://oshwiki.eu/wiki/Detachment_and_recovery_after_work:_An_overview">Detachment and
-                    recovery after work: An overview</a></h3>
-        </div>
-        <div className="publication">
-            <h3><a target="_blank" rel="noopener noreferrer" href="http://oshwiki.eu/wiki/Presenteeism:_An_overview">Presenteeism: An
-                    overview</a></h3>
-        </div>
-        <div className="publication">
-            <h3><a target="_blank" rel="noopener noreferrer"
-                    href="http://oshwiki.eu/wiki/Workaholism_and_the_culture_of_competition:_OSH_consequences">Workaholism
-                    and the culture of competition: OSH consequences</a></h3>
-        </div>
-        <div className="publication">
-            <h3><a target="_blank" rel="noopener noreferrer"
-                    href="http://oshwiki.eu/wiki/The_effects_of_organisational_practices_on_psychosocial_risks">The
-                    effects of strategic organisational change and practices on
-                    psychosocial risks</a></h3>
-        </div>
-        <div className="publication">
-            <h3><a target="_blank" rel="noopener noreferrer"
-                    href="http://oshwiki.eu/wiki/Prevention_of_work-related_violence_to_staff_in_the_education_sector">Prevention
-                    of work-related violence to staff in the education
-                    sector</a></h3>
-        </div>
-        <div className="publication">
-            <h3><a target="_blank" rel="noopener noreferrer" href="http://oshwiki.eu/wiki/Worker_participation_-_United_Kingdom">Worker
-                    participation in the United Kingdom</a></h3>
-        </div>
-        <div className="publication">
-            <h3><a target="_blank" rel="noopener noreferrer"
-                    href="http://oshwiki.eu/wiki/Detachment_and_recovery_after_work:_An_overview">Detachment and
-                    recovery after work: An overview</a></h3>
-        </div>
+            {publications.map(pub => <PublicationCollapse details={pub}/>)}
     </div>
     )
 };
